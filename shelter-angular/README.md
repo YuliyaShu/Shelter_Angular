3f0317e38446463f651bc196fde3af32810643dc
Yuliya_Shulgan <juliette7373@gmail.com>
2023-03-31 12:53:46 +0200

    feat: add routing main and admin

--- shelter-angular/README.md
+++ shelter-angular/README.md
@@ -1,3 +1,7 @@
+BackEnd API https://shelter-qnc2.onrender.com/pets
+BackEnd API documentation https://shelter-qnc2.onrender.com/api
+
+
 # ShelterAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.4.
--- shelter-angular/src/app/app.module.ts
+++ shelter-angular/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainModule } from './core/main/main.module';
+import { AdminModule } from './core/admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MainModule,
+    AdminModule,
     AppRoutingModule
   ],
   providers: [],
