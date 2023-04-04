fc68903485d6624eb63451e9ad0737fa6f62aad9
Yuliya_Shulgan <juliette7373@gmail.com>
2023-04-04 09:33:42 +0200

    feat: add add button on admon page

--- shelter-angular/src/app/core/admin/admin.module.ts
+++ shelter-angular/src/app/core/admin/admin.module.ts
@@ -3,16 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { PetsModule } from 'src/app/shared/pets/pets.module';
-
+import { AddButtonComponent } from './add-button/add-button.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AdminComponent,
+    AddButtonComponent,
   ],
   imports: [
     CommonModule,
     PetsModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatCardModule,
+    MatIconModule,
   ]
 })
 export class AdminModule { }
