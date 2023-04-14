23b17a10687f388c2b812955c07a6981820165e4
Yuliya_Shulgan <juliette7373@gmail.com>
2023-04-14 11:16:44 +0200

    refactor: change api server

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,6 +1,6 @@
 export const CONSTANTS = {
-  // API_URL: "https://shelter-qnc2.onrender.com/pets",
-  API_URL: "https://shelter-angular-crud.vercel.app/pets",
+  API_URL: "https://shelter-qnc2.onrender.com/pets",
+  // API_URL: "https://shelter-angular-crud.vercel.app/pets",
   // API_URL: "http://localhost:3000/pets",
   PET_DEFAULT_NAME: "Bobik",
   PET_DEFAULT_TYPE: "Animal",
